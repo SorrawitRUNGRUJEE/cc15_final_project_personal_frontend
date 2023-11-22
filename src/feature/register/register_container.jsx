4659bef13aa11d2eb93c7439f6c63811589c98d3
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-11-22 14:24:47 +0700

    Finish registerPageUI

--- src/feature/register/register_container.jsx
+++ src/feature/register/register_container.jsx
@@ -1,7 +1,8 @@
 import RegisterForm from "./registerForm";
 
-export default function RegisterContainer(){
-    return <div className=" bg-gray-200 max-w-lg mx-auto  mt-24 py-8 rounded-md min-h-[720px] flex justify-center items-center" >
+export default function RegisterContainer() {
+    return <div className="relative bg-slate-950 min-h-[860px] max-w-3xl min-w-[800px] flex justify-center items-center m-auto  px-4" >
+        <p className="text-white font-semibold text-[40px] absolute top-[-100px] left-0" >Register</p>
         < RegisterForm />
     </div>
 }
--- src/feature/register/register_input.jsx
+++ src/feature/register/register_input.jsx
@@ -1,8 +1,8 @@
-export default function RegisterInput({type,placeholder,name,onChange,hasError}){
+export default function RegisterInput({ type, placeholder, name, onChange, hasError }) {
      return (
-<>
-          
-          < input className={`px-2 py-0.5 rounded-lg ${hasError? "border-2 border-red-500":"border-2 border-solid"}`} type={type|| "text"} placeholder={placeholder||""} onChange={onChange} name={name}/>
-</>
+          <>
+
+               < input className={`mb-4 px-6 py-4 w-[600px] bg-slate-800 shadow-sm text-white border-none outline-none ${hasError ? "border-2 border-red-500" : "border-2 border-solid"}`} type={type || "text"} placeholder={placeholder || ""} onChange={onChange} name={name} />
+          </>
      )
 }
